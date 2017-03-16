0ae6d8506ec8e72cdd3601e4ded07bebd9fa3cd1
R. A. Souza <alves.wm@gmail.com>
2017-03-16 00:16:52 -0300

    changes theme's main colors to fit my logo

--- app/dashboard/home/home.component.ts
+++ app/dashboard/home/home.component.ts
@@ -17,6 +17,6 @@ export class HomeComponent implements OnInit{
         //     var $checkbox = $(this);
         //     $checkbox.checkbox();
         // });
-        initDemo();
+        // initDemo();
     }
 }
