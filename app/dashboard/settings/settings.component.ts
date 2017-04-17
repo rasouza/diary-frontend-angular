0ab1e56e74c768a5df8e172cd1d38da8c71c98aa
R. A. Souza <alves.wm@gmail.com>
2017-04-17 20:45:40 -0300

    adds twitter integration

--- app/dashboard/settings/settings.component.ts
+++ app/dashboard/settings/settings.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { GithubService } from '../../services/github.service';
+import { TwitterService } from '../../services/twitter.service';
 import { env } from "../../env/env.js";
 
 @Component({
@@ -9,32 +10,52 @@ import { env } from "../../env/env.js";
     moduleId: module.id,
     templateUrl: 'settings.component.html',
     styleUrls: ['./settings.component.css'],
-    providers: [GithubService]
+    providers: [GithubService, TwitterService]
 })
 
 export class SettingsComponent implements OnInit{
     repos;
     user;
     logged;
+    twitter_logged;
+    twitter_user;
     redirect_uri = env[env.mode].github_callback;
 
-    constructor(private githubService: GithubService, private route: ActivatedRoute) {}
+    constructor(
+        private githubService: GithubService,
+        private twitterService: TwitterService,
+        private route: ActivatedRoute
+    ) {}
+
     ngOnInit() {
+        this.storeTokens();
+    }
+
+    storeTokens(): void {
         this.route.params.subscribe(params => {
-            if(params['token'])
-                sessionStorage.setItem('token', params['token']);
-
-            if(sessionStorage.getItem('token') !== null) {
-                this.logged = sessionStorage.getItem('token');
-                this.githubService.getUser()
-                    .then(user => this.user = user);
-                this.githubService.getRepos()
-                    .then(repos => this.repos = repos);
-            }
-
-            console.log(sessionStorage.getItem('token'));
+            for(let k in params)
+                sessionStorage.setItem(k, params[k]);
+            this.handleLogin();
         });
     }
 
+    handleLogin(): void {
+        const github_token = sessionStorage.getItem('token');
+        const twitter_token = sessionStorage.getItem('twitter_token');
+        const twitter_token_secret = sessionStorage.getItem('twitter_token_secret');
 
+        if(github_token !== null) this.handleGithub();
+        if(twitter_token !== null) this.handleTwitter();
+    }
+
+    handleGithub(): void {
+        this.logged = sessionStorage.getItem('token');
+        this.githubService.getUser().then(user => this.user = user);
+        this.githubService.getRepos().then(repos => this.repos = repos);
+    }
+
+    handleTwitter(): void {
+        this.twitter_logged = true;
+        this.twitterService.getUser().then(user => this.twitter_user = user);
+    }
 }
---
+++ app/services/twitter.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+
+export class TwitterService {
+    private base: string = 'http://diary-api.dev';
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    constructor(private http: Http) {}
+
+    getUser(): Promise<any> {
+        const auth = {
+            'token': sessionStorage.getItem('twitter_token'),
+            'token_secret': sessionStorage.getItem('twitter_token_secret')
+        }
+        return this.http.post(`${this.base}/api/twitter/user`, JSON.stringify(auth), { headers: this.headers }).toPromise()
+            .then(res => res.json())
+            .then(res => {
+                // Tratamento de imagem
+                const extension = res.profile_image_url.slice(-4);
+                res.profile_image_url = res.profile_image_url.slice(0,-10) + 'bigger' + extension;
+                return res;
+            })
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }
+}
